032c05506d2e981694513044b00477e9dc801b7b
Chanic Panic <51764816+chanicpanic@users.noreply.github.com>
2022-04-02 21:42:44 -0700

    Add configuration options for api factory

--- grascii_rpc/api.py
+++ grascii_rpc/api.py
@@ -1,5 +1,5 @@
 import sys
-from typing import Optional, List
+from typing import Optional, List, Set
 if sys.version_info >= (3, 8):
     from typing import Literal
 else:
@@ -12,38 +12,44 @@ from lark import UnexpectedInput
 
 Strictness = Literal["discard", "retain", "strict"]
 
-def create_api():
+AVAILABLE_APIS = {"grascii.interpret", "grascii.search"}
 
-    api = JSONRPC(service_url="/api", enable_web_browsable_api=True)
+def create_api(service_url: str="/api",
+               enable_web_browsable_api: bool=False,
+               enabled_apis: Set[str]=AVAILABLE_APIS):
+
+    api = JSONRPC(service_url=service_url, enable_web_browsable_api=enable_web_browsable_api)
 
     parser = GrasciiParser()
     searcher = GrasciiSearcher()
 
-    @api.method("grascii.interpret")
-    def interpret(grascii: str) -> Optional[Interpretation]:
-        try:
-            interpretations = parser.interpret(grascii.upper())
-        except UnexpectedInput:
-            return
+    if "grascii.interpret" in enabled_apis:
+        @api.method("grascii.interpret")
+        def interpret(grascii: str) -> Optional[Interpretation]:
+            try:
+                interpretations = parser.interpret(grascii.upper())
+            except UnexpectedInput:
+                return
 
-        return interpretations[0]
+            return interpretations[0]
 
-    @api.method("grascii.search")
-    def search(grascii: str,
-               uncertainty: Literal[0, 1, 2]=0,
-               search_mode: Literal["match", "start", "contain"]="match",
-               annotation_mode: Strictness="discard",
-               aspirate_mode: Strictness="discard",
-               disjoiner_mode: Strictness="strict",
-               fix_first: bool=False,
-               interpretation: Literal["best", "all"]="best") -> List[str]:
-        return searcher.search(grascii=grascii,
-                               uncertainty=uncertainty,
-                               search_mode=search_mode,
-                               annotation_mode=annotation_mode,
-                               aspirate_mode=aspirate_mode,
-                               disjoiner_mode=disjoiner_mode,
-                               fix_first=fix_first,
-                               interpretation=interpretation)
+    if "grascii.search" in enabled_apis:
+        @api.method("grascii.search")
+        def search(grascii: str,
+                   uncertainty: Literal[0, 1, 2]=0,
+                   search_mode: Literal["match", "start", "contain"]="match",
+                   annotation_mode: Strictness="discard",
+                   aspirate_mode: Strictness="discard",
+                   disjoiner_mode: Strictness="strict",
+                   fix_first: bool=False,
+                   interpretation: Literal["best", "all"]="best") -> List[str]:
+            return searcher.search(grascii=grascii,
+                                   uncertainty=uncertainty,
+                                   search_mode=search_mode,
+                                   annotation_mode=annotation_mode,
+                                   aspirate_mode=aspirate_mode,
+                                   disjoiner_mode=disjoiner_mode,
+                                   fix_first=fix_first,
+                                   interpretation=interpretation)
 
     return api
